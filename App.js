c51dbfb509d475dd82e477fb969ec669503d2dd3
Amruda Kandoth <45093030+amrudakandoth@users.noreply.github.com>
2019-05-30 12:43:48 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,106 +1,94 @@
+//This is an example code to understand AsyncStorage// 
 import React, { Component } from 'react';
-import { Text, View, Linking, TouchableHighlight, PermissionsAndroid, Platform, StyleSheet} from 'react-native';
-import { CameraKitCameraScreen, } from 'react-native-camera-kit';
+//import react in our code. 
+import { StyleSheet, View, AsyncStorage, 
+         TextInput, Button, Alert, Text, TouchableOpacity } from 'react-native';
+//import all the components we are going to use. 
 export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      qrvalue: '',
-      opneScanner: false,
+      textInputData: '',
+      //to get the value from the TextInput
+      getValue: '',
+      //to set the value on Text
     };
   }
-  onBarcodeScan(qrvalue) {
-   
-    this.setState({ qrvalue: qrvalue });
-    this.setState({ opneScanner: false });
-  }
-  onOpneScanner() {
-    var that =this;
-    if(Platform.OS === 'android'){
-      async function requestCameraPermission() {
-        try {
-          const granted = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.CAMERA,{
-              'title': 'BarCodeScan App Camera Permission',
-              'message': 'BarCodeScan App needs access to your camera '
-            }
-          )
-          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-            that.setState({ qrvalue: '' });
-            that.setState({ opneScanner: true });
-          } else {
-            alert("CAMERA permission denied");
-          }
-        } catch (err) {
-          alert("Camera permission err",err);
-          console.warn(err);
-        }
-      }
-      requestCameraPermission();
+  saveValueFunction = () => {
+    //function to save the value in AsyncStorage
+    if(this.state.textInputData){
+      //To check the input not empty
+      AsyncStorage.setItem('any_key_here', this.state.textInputData);
+      //Setting a data to a AsyncStorage with respect to a key 
+      this.setState({ textInputData: '' })
+      //Resetting the TextInput
+      alert('Data Saved');
+      //alert to confirm
     }else{
-      that.setState({ qrvalue: '' });
-      that.setState({ opneScanner: true });
-    }    
-  }
-  render() {
-    let displayModal;
-    if (!this.state.opneScanner) {
-      return (
-        <View style={styles.container}>
-            <Text style={styles.heading}>React Native QR Code </Text>
-            <Text style={styles.simpleText}>{this.state.qrvalue ? 'Scanned QR Code: '+this.state.qrvalue : ''}</Text>
-            <TouchableHighlight
-              onPress={() => this.onOpneScanner()}
-              style={styles.button}>
-                <Text style={{ color: '#FFFFFF', fontSize: 12 }}>
-                Open QR Scanner
-                </Text>
-            </TouchableHighlight>
-        </View>
-      );
+      alert('Please fill data');
+      //alert for the empty InputText
     }
+  };
+  getValueFunction = () => {
+    //function to get the value from AsyncStorage
+    AsyncStorage.getItem('any_key_here').then(value =>
+      //AsyncStorage returns a promise so adding a callback to get the value
+      this.setState({ getValue: value })
+      //Setting the value in Text 
+    );
+  };
+  render() {
     return (
-      <View style={{ flex: 1 }}>
-        <CameraKitCameraScreen
-          showFrame={false}
-          scanBarcode={true}
-          laserColor={'blue'}
-          frameColor={'yellow'}
-          colorForScannerFrame={'black'}
-          onReadCode={event =>
-            this.onBarcodeScan(event.nativeEvent.codeStringValue)
-          }
+      <View style={styles.MainContainer}>
+        <TextInput
+          placeholder="Enter Some Text here"
+          value={this.state.textInputData}
+          onChangeText={data => this.setState({ textInputData: data })}
+          underlineColorAndroid="transparent"
+          style={styles.TextInputStyle}
         />
+        <TouchableOpacity
+          onPress={this.saveValueFunction}
+          style={styles.button}>
+          <Text style={styles.buttonText}> SAVE VALUE </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={this.getValueFunction}
+          style={styles.button}>
+          <Text style={styles.buttonText}> GET VALUE </Text>
+        </TouchableOpacity>
+        <Text style={styles.text}> {this.state.getValue} </Text>
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  MainContainer: {
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor:'white'
+    flex: 1,
+    margin: 10,
+    marginTop:60
+  },
+  TextInputStyle: {
+    textAlign: 'center',
+    height: 40,
+    width: '100%',
+    borderWidth: 1,
+    borderColor: '#808000',
   },
   button: {
-    alignItems: 'center',
-    backgroundColor: '#2c3539',
+    width: '100%',
+    height: 40,
     padding: 10,
-    width:300,
-    marginTop:16
+    backgroundColor: '#808000',
+    marginTop: 10,
   },
-  heading: { 
-    color: 'black', 
-    fontSize: 24, 
-    alignSelf: 'center', 
-    padding: 10, 
-    marginTop: 30 
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
   },
-  simpleText: { 
-    color: 'black', 
-    fontSize: 20, 
-    alignSelf: 'center', 
-    padding: 10, 
-    marginTop: 16
-  }
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
 });
